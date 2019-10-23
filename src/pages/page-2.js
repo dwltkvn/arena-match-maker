e31a073d9f6520570335d7388196b9561fdc24e4
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2019-10-23 22:18:44 +0200

    add register/unregister to firebase
    also toogle the gui given the registration state

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -12,12 +12,17 @@ class SecondPage extends React.Component {
   constructor(props) {
     super(props)
     // this.handeEvent = this.handleEvent.bind(this);
-    this.state = {}
+    this.register = this.register.bind(this)
+    this.unregister = this.unregister.bind(this)
+    this.state = {
+      stateUserRegistered: false,
+    }
   }
 
   componentDidMount() {
     //window.addEvetListen("event", this.handleEvent);
     console.log("mount")
+    console.log(this.state.stateUserRegistered)
     const v = firebase
       .database()
       .ref("/username/")
@@ -36,6 +41,37 @@ class SecondPage extends React.Component {
      ...
   }*/
 
+  register() {
+    //firebase.ref
+    const username = this.refUserInput.value
+    const mode = this.refModeSelect.value
+    const fbpath = `${mode}/${username}`
+    console.log(fbpath)
+    console.log(username)
+    console.log(mode)
+    firebase
+      .database()
+      .ref(fbpath)
+      .set({ opponent: 0 })
+      .then(() => this.setState({ stateUserRegistered: true }))
+      .catch(error => this.setState({ stateUserRegistered: false }))
+  }
+
+  unregister() {
+    const username = this.refUserInput.value
+    const mode = this.refModeSelect.value
+    const fbpath = `${mode}/${username}`
+    console.log(fbpath)
+    console.log(username)
+    console.log(mode)
+    firebase
+      .database()
+      .ref(fbpath)
+      .set({})
+      .then(() => this.setState({ stateUserRegistered: false }))
+      .catch(error => this.setState({ stateUserRegistered: true }))
+  }
+
   render() {
     //const {classes} = this.props;
     //const {myState} = this.state;
@@ -45,9 +81,19 @@ class SecondPage extends React.Component {
         <h1>Hi from the second page</h1>
         <p>Welcome to Match Maker</p>
         <div>
-          <input type="text" id="text_id" placeholder="Username" />
+          <input
+            type="text"
+            id="text_id"
+            placeholder="Username"
+            ref={input => (this.refUserInput = input)}
+            disabled={this.state.stateUserRegistered}
+          />
           <div>
-            <select name="mode">
+            <select
+              name="mode"
+              ref={select => (this.refModeSelect = select)}
+              disabled={this.state.stateUserRegistered}
+            >
               <option value="brawl">Brawl</option>
               <option value="jank">Jank</option>
               <option value="pauper">Pauper</option>
@@ -55,8 +101,20 @@ class SecondPage extends React.Component {
             </select>
           </div>
           <div>
-            <button type="button">Register</button>
-            <button type="button">Unregister</button>
+            <button
+              type="button"
+              onClick={() => this.register()}
+              disabled={this.state.stateUserRegistered}
+            >
+              Register
+            </button>
+            <button
+              type="button"
+              onClick={() => this.unregister()}
+              disabled={!this.state.stateUserRegistered}
+            >
+              Unregister
+            </button>
           </div>
         </div>
         <Link to="/">Go back to the homepage</Link>
